ea24ce180c8e62e82559b6072668558b45088ac2
jay <jay@imac.local>
2022-04-24 22:55:31 +0800

    refactor: prepare for wx component

--- lib/component.service.ts
+++ lib/component.service.ts
@@ -2,4 +2,25 @@ import { WeChatModuleOptions } from './types';
 
 export class Component {
   constructor (private options: WeChatModuleOptions) {}
+  // 解密推送ticket
+  // https://developers.weixin.qq.com/doc/oplatform/Third-party_Platforms/2.0/api/ThirdParty/token/component_verify_ticket.html
+
+  // 获取令牌
+  // https://developers.weixin.qq.com/doc/oplatform/Third-party_Platforms/2.0/api/ThirdParty/token/component_access_token.html
+
+  // 获取预授权码
+  // https://developers.weixin.qq.com/doc/oplatform/Third-party_Platforms/2.0/api/ThirdParty/token/pre_auth_code.html
+
+  // 使用授权码获取授权信息
+  // https://developers.weixin.qq.com/doc/oplatform/Third-party_Platforms/2.0/api/ThirdParty/token/authorization_info.html
+  // 步骤五、管理员授权确认之后，授权页会自动跳转进入回调 URI，并在 URL 参数中返回授权码和过期时间(redirect_url?auth_code=xxx&expires_in=600)。
+  
+  // 获取/刷新接口调用令牌
+  // https://developers.weixin.qq.com/doc/oplatform/Third-party_Platforms/2.0/api/ThirdParty/token/api_authorizer_token.html
+
+  // 获取授权帐号详情
+  // https://developers.weixin.qq.com/doc/oplatform/Third-party_Platforms/2.0/api/ThirdParty/token/api_get_authorizer_info.html
+
+
+
 }
